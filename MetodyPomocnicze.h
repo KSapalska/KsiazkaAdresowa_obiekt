57edebb1a71aa1750e0b2b39ccd66a2ab32bf66f
Kasia <kasiasapalska@gmail.com>
2022-04-13 16:55:50 +0200

    Divide KsiazkaAdresowa to smaller class

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,19 @@
+#ifndef KSIAZKAADRESOWA_H
+#define KSIAZKAADRESOWA_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+
+
+ class MetodyPomocnicze
+ {
+public:
+    static int konwersjaStringNaInt(string liczba);
+    static string konwerjsaIntNaString(int liczba);
+
+ };
+
+ #endif
